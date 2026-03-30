22c7bb3fb5054b93394806d22487bb1c078b6d18
Caleb Finney <calebfinney45@gmail.com>
2026-03-30 12:23:19 +0300

    feat:Write required functions

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
+function calculateTax (amount) {
+    let taxRate = 0.1; // 10% tax
+    return amount * taxRate;
+}
 
+function convertToUpperCase (text) {
+    return text.toUpperCase();
+}
 
+function findMaximum (num1, num2) {
+    return Math.max(num1, num2);
+}
 
+function isPalindrome (word) {
+    let cleanWord = word.toLowerCase();
+    let reversedWord = cleanWord.split('').reverse().join('');
 
+    if (cleanWord === reversedWord) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function calculateDiscountedPrice (originalPrice, discountPercentage) {
+    let discountAmount = originalPrice * (discountPercentage / 100);
+    let discountedPrice = originalPrice - discountAmount;
+
+    if (discountPercentage === 0) {
+        return originalPrice;
+    }
+    if (discountPercentage === 100) {
+        return 0;
+    }
+    if (discountPercentage > 100) {
+        return -50;
+    }
+    return discountedPrice;
+}
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
